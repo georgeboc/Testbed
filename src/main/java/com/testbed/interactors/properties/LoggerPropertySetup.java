aba157ef92c492fa3bc0c775bc0204662ecf08a9
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-05-14 23:09:28 +0200

    Writes logs locally in the tmp folder and uploads them to google drive

--- src/main/java/com/testbed/interactors/properties/LoggerPropertySetup.java
+++ src/main/java/com/testbed/interactors/properties/LoggerPropertySetup.java
@@ -1,13 +1,19 @@
 package com.testbed.interactors.properties;
 
+import lombok.SneakyThrows;
+
+import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LoggerPropertySetup {
     private static final String CURRENT_TIMESTAMP = "current_timestamp";
+    private static final String HOSTNAME = "hostname";
 
+    @SneakyThrows
     public static void setup() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh_mm_ss-SS");
+        System.setProperty(HOSTNAME, InetAddress.getLocalHost().getHostName());
         System.setProperty(CURRENT_TIMESTAMP, dateFormat.format(new Date()));
     }
 }
