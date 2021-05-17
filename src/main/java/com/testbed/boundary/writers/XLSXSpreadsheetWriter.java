2cf00f64d72b051d0b82e302d94e89b4ac755a15
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-05-17 20:52:52 +0200

    Changes output excel font

--- src/main/java/com/testbed/boundary/writers/XLSXSpreadsheetWriter.java
+++ src/main/java/com/testbed/boundary/writers/XLSXSpreadsheetWriter.java
@@ -7,10 +7,10 @@ import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -28,6 +28,10 @@ import java.util.Optional;
 public class XLSXSpreadsheetWriter implements SpreadsheetWriter {
     private static final String EMPTY = "";
     private static final boolean USE_UNMERGED_CELLS = true;
+    private static final String DEFAULT_FONT_NAME = "Verdana";
+    private static final short DEFAULT_FONT_HEIGHT = 10;
+    private static final short DEFAULT_FONT_COLOR_INDEX = IndexedColors.BLACK.getIndex();
+
 
     private final FileSystem fileSystem;
 
@@ -36,7 +40,8 @@ public class XLSXSpreadsheetWriter implements SpreadsheetWriter {
         Workbook workbook = tryGetWorkbook(outputParameters);
         Sheet sheet = getOrCreateSheet(outputParameters, workbook);
         Row row = getOrCreateRow(sheet, position.getRow());
-        CellUtil.createCell(row, position.getColumn(), value);
+        Cell cell = CellUtil.createCell(row, position.getColumn(), value);
+        setDefaultFont(workbook, cell);
         sheet.autoSizeColumn(position.getColumn());
         tryWriteWorkbook(outputParameters, workbook);
     }
@@ -49,6 +54,7 @@ public class XLSXSpreadsheetWriter implements SpreadsheetWriter {
         Cell cell = CellUtil.createCell(row, position.getColumn(), value);
         CellUtil.setCellStyleProperty(cell, workbook, CellUtil.FILL_PATTERN, CellStyle.SOLID_FOREGROUND);
         CellUtil.setCellStyleProperty(cell, workbook, CellUtil.FILL_FOREGROUND_COLOR, IndexedColors.valueOf(colorName).getIndex());
+        setDefaultFont(workbook, cell);
         sheet.autoSizeColumn(position.getColumn());
         tryWriteWorkbook(outputParameters, workbook);
     }
@@ -61,6 +67,7 @@ public class XLSXSpreadsheetWriter implements SpreadsheetWriter {
         Cell cell = CellUtil.createCell(row, position.getColumn(), EMPTY);
         cell.setCellFormula(formula);
         HSSFFormulaEvaluator.evaluateAllFormulaCells(workbook);
+        setDefaultFont(workbook, cell);
         sheet.autoSizeColumn(position.getColumn(), USE_UNMERGED_CELLS);
         tryWriteWorkbook(outputParameters, workbook);
     }
@@ -94,6 +101,7 @@ public class XLSXSpreadsheetWriter implements SpreadsheetWriter {
         Row row = sheet.getRow(startPosition.getRow());
         Cell cell = CellUtil.getCell(row, startPosition.getColumn());
         CellUtil.setCellStyleProperty(cell, workbook, CellUtil.ALIGNMENT, CellStyle.ALIGN_CENTER);
+        setDefaultFont(workbook, cell);
         tryWriteWorkbook(outputParameters, workbook);
     }
 
@@ -103,6 +111,14 @@ public class XLSXSpreadsheetWriter implements SpreadsheetWriter {
         Optional<Sheet> optionalSheet = Optional.ofNullable(workbook.getSheet(outputParameters.getSheetName()));
         optionalSheet.ifPresent(sheet -> workbook.removeSheetAt(workbook.getSheetIndex(sheet)));
         tryWriteWorkbook(outputParameters, workbook);
+    }
+
+    private void setDefaultFont(Workbook workbook, Cell cell) {
+        Font font = workbook.getFontAt(cell.getCellStyle().getFontIndex());
+        font.setFontName(DEFAULT_FONT_NAME);
+        font.setFontHeightInPoints(DEFAULT_FONT_HEIGHT);
+        font.setColor(DEFAULT_FONT_COLOR_INDEX);
+        CellUtil.setFont(cell, workbook, font);
     }
 
     private Sheet getOrCreateSheet(OutputParameters outputParameters, Workbook workbook) {
